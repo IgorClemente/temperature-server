54e6770ac6515adb51befa0a4563bfc4fbd167fe
Igor Clemente <igorelectrofest2011@>
2019-09-20 00:11:35 -0300

    BOOTCAMP_GOSTACK_APPCSS_CREATED_18092019

--- Raspberry Pi/humidity.py
+++ Raspberry Pi/humidity.py
@@ -40,7 +40,7 @@ bottom = height-padding
 x = 0
 
 font = ImageFont.load_default()
-font = ImageFont.truetype('Minecraftia-Regular.ttf', 8)
+font = ImageFont.truetype('Minecraftia-Regular.ttf', 16)
 
 def dummy() :
     DHT_SENSOR = Adafruit_DHT.DHT22
