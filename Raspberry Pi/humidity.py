4434199a573cd2768ad7cf97986ba531bf6ed9b9
Igor Clemente <igorelectrofest2011@>
2019-09-19 23:13:21 -0300

    BOOTCAMP_GOSTACK_APPCSS_CREATED_18092019

--- Raspberry Pi/humidity.py
+++ Raspberry Pi/humidity.py
@@ -1,7 +1,47 @@
 import Adafruit_DHT
 import time
 
-   
+import time
+
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_SSD1306
+
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+import subprocess
+
+RST = None    
+
+DC = 23
+SPI_PORT = 0
+SPI_DEVICE = 0
+
+disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
+
+disp.begin()
+
+disp.clear()
+disp.display()
+
+width = disp.width
+height = disp.height
+image = Image.new('1', (width, height))
+
+draw = ImageDraw.Draw(image)
+
+draw.rectangle((0,0,width,height), outline=0, fill=0)
+
+padding = -2
+top = padding
+bottom = height-padding
+
+x = 0
+
+font = ImageFont.load_default()
+
+
 def dummy() :
     DHT_SENSOR = Adafruit_DHT.DHT22
     DHT_PIN = 4
@@ -11,6 +51,15 @@ def dummy() :
         humidity,temp = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
         temperature = temp 
         time.sleep(0.1)
+
+    draw.rectangle((0,0,width,height), outline=0, fill=0)
+
+    draw.text((x, top+16), str(temperature) + "°C",  font=font, fill=255)
+
+    disp.image(image)
+    disp.display()
+    time.sleep(.1)
+
     print (temperature)
     temperature = None
 if __name__ =='__main__' :
