a00da399b29ca03c993d369209093c8c8a3c9332
Igor Clemente <igorelectrofest2011@>
2019-09-19 21:49:29 -0300

    BOOTCAMP_GOSTACK_APPCSS_CREATED_18092019

--- Raspberry Pi/server.js
+++ Raspberry Pi/server.js
@@ -5,11 +5,6 @@ const server = express();
 
 const { PythonShell } = require("python-shell");
 
-var fs = require("fs");
-var sys = require("sys");
-var spawn = require("child_process").spawn;
-var dummy = spawn("python", ["humidity.py"]);
-
 var serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
@@ -22,21 +17,13 @@ var ref = defaultDatabase.ref();
 
 entitiesFromAssistant = {};
 
-/** 
-dummy.stdout.on("data", function(data) {
-  ref.set({ temperature: data.toString() });
-});
-**/
-
-PythonShell.run("humidity.py", null, function(err, result) {
-  if (err) throw err;
-  console.log("finished", result);
-
+setInterval(() => {
   PythonShell.run("humidity.py", null, function(err, result) {
     if (err) throw err;
     console.log("finished", result);
+    ref.set({ temperature: result });
   });
-});
+}, 20000);
 
 ref.on("value", function(snapshot) {
   snapshot.forEach(function(childSnapshot) {
@@ -45,6 +32,5 @@ ref.on("value", function(snapshot) {
 
     entitiesFromAssistant[key] = val;
   });
-
   console.log(entitiesFromAssistant);
 });
